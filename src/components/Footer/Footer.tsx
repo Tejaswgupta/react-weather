e45f61bc9a27bba158659d2933af4364f48fd53f
Tejasw Gupta <guptatejasw2002@gmail.com>
2021-06-14 20:31:28 +0530

    Initial rebased commit

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FooterContainer } from "./styled";
+
+const Footer: React.FC = () => {
+  return (
+    <FooterContainer>
+      <p>
+        Developed By <a href="https://www.tejaswagupta.me">Tejaswa Gupta</a>
+      </p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
